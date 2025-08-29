75c1fb1b60983537e02d0aa7ac47f675cfb8c2ca
Gilmer Contreras <ivsmca90@gmail.com>
2025-08-28 22:34:10 -0400

    rifacaracasyei

--- middleware/preventHotlink.js
+++ middleware/preventHotlink.js
@@ -1,10 +1,10 @@
 // middleware/preventHotlink.js
 module.exports = function (req, res, next) {
   const allowedReferers = [
-    'https://autoslaplaya.com',
-    'https://www.autoslaplaya.com',
-    'https://admin.rifalaplaya.com',
-    'https://www.admin.rifalaplaya.com',
+    'https://rifacaracasyei.com',
+    'https://www.rifacaracasyei.com',
+    'https://admin.rifacaracasyei.com',
+    'https://www.admin.rifacaracasyei.com',
   ];
 
   const url = req.originalUrl || '';
