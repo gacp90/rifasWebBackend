5305e62e5f965be2e51dceb637839a86ff2d999d
Gilmer Contreras <ivsmca90@gmail.com>
2025-09-29 10:55:34 -0400

    rifacaracasyei

--- middleware/preventHotlink.js
+++ middleware/preventHotlink.js
@@ -1,10 +1,10 @@
 // middleware/preventHotlink.js
 module.exports = function (req, res, next) {
   const allowedReferers = [
-    'https://rifasgranoriente.com',
-    'https://www.rifasgranoriente.com',
-    'https://admin.rifasgranoriente.com',
-    'https://www.admin.rifasgranoriente.com',
+    'https://rifacaracasyei.com',
+    'https://www.rifacaracasyei.com',
+    'https://admin.rifacaracasyei.com',
+    'https://www.admin.rifacaracasyei.com',
   ];
 
   const url = req.originalUrl || '';
