30d3550f2f6b24542cb137753bf143417d640e0b
Gilmer Contreras <ivsmca90@gmail.com>
2025-11-10 22:15:45 -0400

    grupolosmagicos

--- middleware/preventHotlink.js
+++ middleware/preventHotlink.js
@@ -1,10 +1,10 @@
 // middleware/preventHotlink.js
 module.exports = function (req, res, next) {
   const allowedReferers = [
-    'https://rifasgranoriente.com',
-    'https://www.rifasgranoriente.com',
-    'https://admin.rifasgranoriente.com',
-    'https://www.admin.rifasgranoriente.com',
+    'https://grupolosmagicos.com',
+    'https://www.grupolosmagicos.com',
+    'https://admin.grupolosmagicos.com',
+    'https://www.admin.grupolosmagicos.com',
   ];
 
   const url = req.originalUrl || '';
