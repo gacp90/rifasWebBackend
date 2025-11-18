cb02d33dea100d6c19ef618a7d9428bd42e838b9
Gilmer Contreras <ivsmca90@gmail.com>
2025-11-18 09:26:22 -0400

    rifasmcg

--- middleware/preventHotlink.js
+++ middleware/preventHotlink.js
@@ -1,10 +1,10 @@
 // middleware/preventHotlink.js
 module.exports = function (req, res, next) {
   const allowedReferers = [
-    'https://rifacaracasyei.com',
-    'https://www.rifacaracasyei.com',
-    'https://admin.rifacaracasyei.com',
-    'https://www.admin.rifacaracasyei.com',
+    'https://rifasmcg.com',
+    'https://www.rifasmcg.com',
+    'https://admin.rifasmcg.com',
+    'https://www.admin.rifasmcg.com',
   ];
 
   const url = req.originalUrl || '';
