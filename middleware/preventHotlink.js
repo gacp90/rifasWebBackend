8c9e59a9e4fcbc5754216f8029c40ab16a034970
Gilmer Contreras <ivsmca90@gmail.com>
2025-12-07 15:57:03 -0400

    mcg

--- middleware/preventHotlink.js
+++ middleware/preventHotlink.js
@@ -1,10 +1,10 @@
 // middleware/preventHotlink.js
 module.exports = function (req, res, next) {
   const allowedReferers = [
-    'https://rifaexpres.com',
-    'https://www.rifaexpres.com',
-    'https://admin.rifaexpres.com',
-    'https://www.admin.rifaexpres.com',
+    'https://rifasmcg.com',
+    'https://www.rifasmcg.com',
+    'https://admin.rifasmcg.com',
+    'https://www.admin.rifasmcg.com',
   ];
 
   const url = req.originalUrl || '';
